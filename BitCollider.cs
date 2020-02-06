4d282424116f8937c259e601aeded4411faed373
cbdix92 <cbdix92@gmail.com>
2020-02-06 11:16:56 -0500

    Update

--- BitCollider.cs
+++ BitCollider.cs
@@ -6,61 +6,64 @@ using System.Threading.Tasks;
 
 namespace CMDR
 {
-    internal static class BitCollider
+    public static class BitCollider
     {
         public static byte AlphaThreshold = 60;
 
-        public static bool BitCollide(GameObject gameObject, GameObject collider)
+        internal static bool BitCollide(GameObject gameObject, GameObject collider)
         {
-            bool result = false;
+            bool Result = false;
+			
+			float OBJ1X = gameObject.Transform.X;
+			float OBJ1Y = gameObject.Transform.Y;
+			
+			float OBJ2X = collider.Transform.X;
+			float OBJ2Y = collider.Transform.Y;
 
             int P1X = (int)Math.Min(gameObject.Transform.X, collider.Transform.X);
-            int P2X = (int)Math.Max(gameObject.Transform.X, collider.Transform.X);
-
-            int P1Y = (int)Math.Min(gameObject.Transform.Y, collider.Transform.Y);
+			int P1Y = (int)Math.Min(gameObject.Transform.Y, collider.Transform.Y);
+            
+			int P2X = (int)Math.Max(gameObject.Transform.X, collider.Transform.X);
             int P2Y = (int)Math.Max(gameObject.Transform.Y, collider.Transform.Y);
 
             int BoundSize = (P2X - P1X) * (P2Y - P1Y);
 
-            int BoundWidth = P2X - P1X;
-            int BoundHeight = P2Y - P1Y;
+            int Width = P2X - P1X;
+			
+			CollisionData ColData1 = gameObject.PhysicsConstraints.ColliderData;
+			CollisionData ColData2 = collider.PhysicsConstraints.ColliderData;
 
-            for (int i = 0; i < BoundSize; i++)
+            for (int i = 0; i < BoundSize - 1; i++)
             {
-                if (i <= BoundWidth - 1)
-                    break;// To be continued ...
+                if (ColData1.Data[(i / Width) - P1Y - OBJ1Y,(i % Width) - P1X - OBJ1X] 
+					&& 
+					ColData2.Data[(i / Width) - P1Y - OBJ1Y,(i % Width) - P1X - OBJ2X])
+					Result = true;
             }
 
 
 
-            return result;
+            return Result;
         }
     }
     internal class CollisionData
     {
-        public bool[] Data;
+        public bool[,] Data;
+		
         public CollisionData(System.Drawing.Image image)
         {
             int Width = image.Width;
+			int Height = image.Height;
 
-            int DataSize = Width * image.Height;
-
-
-            Data = new bool[DataSize];
+            Data = new bool[Height, Width];
 
             System.Drawing.Bitmap Bitmap = new System.Drawing.Bitmap(image);
 
-            for (int i = 0; i <= DataSize - 1; i++)
-            {
-                if (i >= Width - 1)
-                {
-                    Data[i] = Bitmap.GetPixel(i % Width, Math.Abs(i / Width)).A > BitCollider.AlphaThreshold;
-                }
-                else
-                {
-                    Data[i] = Bitmap.GetPixel(i % Width, 0).A > BitCollider.AlphaThreshold;
-                }
-            }
+            for (int Y = 0; Y <= Height; Y++)
+				for (int X = 0; X <= Width; X++)
+				{
+					Data[Y,X] = Bitmap.GetPixel(X,Y).A > BitCollider.AlphaThreshold;
+				}
         }
     }
 }
