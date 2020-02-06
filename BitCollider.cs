42bd4910be5ec625d8622d4cd3540c7fede9fa85
cbdix92 <cbdix92@gmail.com>
2020-02-05 20:08:22 -0500

    Update

--- BitCollider.cs
+++ BitCollider.cs
@@ -9,24 +9,57 @@ namespace CMDR
     internal static class BitCollider
     {
         public static byte AlphaThreshold = 60;
+
+        public static bool BitCollide(GameObject gameObject, GameObject collider)
+        {
+            bool result = false;
+
+            int P1X = (int)Math.Min(gameObject.Transform.X, collider.Transform.X);
+            int P2X = (int)Math.Max(gameObject.Transform.X, collider.Transform.X);
+
+            int P1Y = (int)Math.Min(gameObject.Transform.Y, collider.Transform.Y);
+            int P2Y = (int)Math.Max(gameObject.Transform.Y, collider.Transform.Y);
+
+            int BoundSize = (P2X - P1X) * (P2Y - P1Y);
+
+            int BoundWidth = P2X - P1X;
+            int BoundHeight = P2Y - P1Y;
+
+            for (int i = 0; i < BoundSize; i++)
+            {
+                if (i <= BoundWidth - 1)
+                    break;// To be continued ...
+            }
+
+
+
+            return result;
+        }
     }
-    internal class CollidionData
+    internal class CollisionData
     {
-        public bool[,] Data;
-        public CollidionData(System.Drawing.Image image)
+        public bool[] Data;
+        public CollisionData(System.Drawing.Image image)
         {
             int Width = image.Width;
-            int Height = image.Height;
 
-            Data = new bool[Height, Width];
+            int DataSize = Width * image.Height;
+
+
+            Data = new bool[DataSize];
 
             System.Drawing.Bitmap Bitmap = new System.Drawing.Bitmap(image);
 
-            for (int Y = 0; Y <= Height; Y++)
-                for (int X =0; X <= Width; X++)
+            for (int i = 0; i <= DataSize; i++)
+            {
+                if (i <= Width - 1)
                 {
-                    Data[Y,X] = Bitmap.GetPixel(X, Y).A > BitCollider.AlphaThreshold;
+                    Data[i] = Bitmap.GetPixel(i % Width - 1, 0).A > BitCollider.AlphaThreshold;
+                    continue;
                 }
+
+                Data[i] = Bitmap.GetPixel(i % Width - 1, Math.Abs(i / Width)).A > BitCollider.AlphaThreshold;
+            }
         }
     }
 }
